4e63f343d040b3b88f245c5e8322af538bda1acd
Julien Cromback <crombackjulien@gmail.com>
2021-02-10 15:31:22 +0100

    Solved exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :restaurants, only: [ :index, :show, :new, :create ] do
+    resources :reviews, only: [ :create ]
+  end
+
   # root to: "controller#action"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" , phone_number: "0764646437"}
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "french" , phone_number: "0764646437" }
+
+
+[ dishoom, pizza_east ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+review = {content: "Great Restaurant", rating: "4", restaurant_id: 13}
+Review.create!(review)
+
+
+
+puts "Finished!"
